c3c25e7af29ab5b78b40e49af5b81e661af5b309
neifelheim <b0rn2c0d3@gmail.com>
2013-06-25 01:02:50 +0530

    Add last post information to topics index

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -4,7 +4,8 @@ module Forem
 
     attr_accessible :subject, :user, :posts_attributes
     has_many :posts, :order => "created_at ASC"
-    belongs_to :user, :class_name => Forem::Engine.user_class.to_s    
+    belongs_to :user, :class_name => Forem::Engine.user_class.to_s
+    has_one :last_post, :class_name => "Forem::Post", :order => "created_at DESC"
     accepts_nested_attributes_for :posts
 
     private
